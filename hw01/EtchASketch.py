5077c99513062dad82666943081a76eab1aac8a1
boxetham <boxetham@BOXETHAM-W541.rose-hulman.edu>
2018-09-06 23:41:39 -0400

    homework 1 completed

--- hw01/EtchASketch.py
+++ hw01/EtchASketch.py
@@ -12,13 +12,15 @@ print("The O is where the curser is currently")
 while True:  # making a loop
     # print board here
     if somethingChanged:
+        # print the first row of numbers
         sys.stdout.write("  ")
         for x in range(0, len(board)):
             sys.stdout.write(`x + 1` + " ")
         sys.stdout.write("\n")
+        # print the actual grid
         for x in range(0, len(board)):
             for y in range(0, len(board[x])):
-                if y == 0:
+                if y == 0: # for printing the column numbers
                     sys.stdout.write(`x + 1` + " ")
                 if x == row and y == col:
                     sys.stdout.write("O ")
@@ -29,25 +31,29 @@ while True:  # making a loop
             sys.stdout.write("\n")
         sys.stdout.flush()
         somethingChanged = False
-    inputChar = sys.stdin.read(1)
-    board[row][col] = 1
-    if inputChar == 'u':  
+        
+    inputChar = sys.stdin.read(1) # get keyboard input
+
+    board[row][col] = 1 # marks the current spot before moving
+
+    # check which way the user wants to move
+    if inputChar == 'u': # up
         if row != 0:
             row = row - 1
         somethingChanged = True
-    if inputChar == 'd':
+    if inputChar == 'd': # down
         if row != 7:  
             row = row + 1
         somethingChanged = True
-    if inputChar == 'r':  
+    if inputChar == 'r': # right
         if col != 7:
             col = col + 1
         somethingChanged = True
-    if inputChar == 'l':  
+    if inputChar == 'l': # left
         if col != 0:
             col = col - 1
         somethingChanged = True
-    if inputChar == 'c':
+    if inputChar == 'c': # clear the board
         for x in range(0, len(board)):
             for y in range(0, len(board[x])):
                 board[x][y] = 0
---
+++ hw01/ReadMe.md.txt
@@ -0,0 +1,15 @@
+I did all of the six items including: 
+
+1. installing the latest shipped image on the BeagleBone
+2. setting up a host computer with Linux for kernel development
+3. gathering all the needed SD cards, cables, etc.
+4. installing git on your host 5. signing up for the two beagle Google groups
+6. writing a simple Etch-a-sketch program
+
+For (6) the program will output the grid to the console as you move using your key board.
+The instructions are as follows (same as what is printed out when you run the program): 
+
+Move by pressing the keyboard button corresponding to the way you 
+want to move and then hit enter
+Directions: u - up, d - down, r - right, l - left, c - clear the board
+The O is where the curser currently is
