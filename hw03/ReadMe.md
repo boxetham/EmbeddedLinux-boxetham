7328195bf2e2d96ded7b146b856001284554c8f2
Demo User <debian@beaglebone.localdomain>
2018-09-17 18:35:42 +0000

    homework 3 finished

---
+++ hw03/ReadMe.md
@@ -0,0 +1,6 @@
+Left/Right encoder on eQEP1
+Up/Down encoder on eQEP2
+Clear button on P9_42
+Stop button on P9_23
+
+temp sensor script works
--- hw03/tempscript.sh
+++ hw03/tempscript.sh
@@ -1,7 +1,7 @@
-temp=`i2cget -y 1 0x48`
-temp1=`i2cget -y 1 0x4A`
-tempout=$(($temp*1.8 + 32))
-temp2out=$(($temp1*1.8 + 32))
+temp=`i2cget -y 2 0x48`
+temp1=`i2cget -y 2 0x4A`
+tempout=$(($temp*9/5 + 32))
+temp2out=$(($temp1*9/5 + 32))
 
-echo $tempout
-echo $temp2out
+echo Temp1 is $tempout
+echo Temp2 is $temp2out
